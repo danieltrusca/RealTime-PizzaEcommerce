37b6f9f32f001f2e7c0938f7e7ab490531cdae02
Dan Trusca <truscadani@yahoo.com>
2020-12-02 12:20:47 +0200

    production build

--- server.js
+++ server.js
@@ -74,6 +74,9 @@ app.set("views", path.join(__dirname, "/resources/views"));
 app.set("view engine", "ejs");
 
 require("./routes/web")(app);
+app.use((req, res) => {
+  res.status(404).render("errors/404");
+});
 
 const server = app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
